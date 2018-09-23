ee7021212aee911030707c3b238c59294e2b3bc9
hail2m15 <hail2m15@gmail.com>
2018-09-23 13:14:33 +0530

    dashboard updated

---
+++ app/views/common/head.php
@@ -0,0 +1,56 @@
+<div class="wrapper">
+    <div class="sidebar" data-color="red" data-image="../images/sidebar-5.jpg">
+
+        <!--   you can change the color of the side bar using: data-color="blue | azure | green | orange | red | purple" -->
+
+
+        <div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="home" class="simple-text">
+                    <img src="../images/logo.jpg" alt="wedding" style="width: 169px;height: 100px;"/>
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li  class="active">
+                    <a href="home">
+                        <i class="fa fa-book"></i>
+                        <p>BOOKING</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="followup">
+                        <i class="fa fa-user"></i>
+                        <p>FOLLOWUP</p>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <div class="main-panel">
+        <nav class="navbar navbar-default navbar-fixed">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand head" href="#" style="color:#ee2d20;">EVENT MANAGEMENT</a>
+                </div>
+                                <div class="collapse navbar-collapse">
+
+
+                    <ul class="nav navbar-nav navbar-right">
+
+                        <li>
+                            <a class="log" href="logout">
+                                <p style="font-size:14px;">LOGOUT&nbsp;&nbsp;<i class="glyphicon glyphicon-off"></i></p>
+                            </a>
+                        </li>
+                        <li class="separator hidden-lg hidden-md"></li>
+                    </ul>
+                </div>
+
+        </nav>
